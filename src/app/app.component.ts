7dd856716571d07fc076961ce05c20c865ba6dc6
Filippo Bodini <filippo.bodini@softwave.it>
2021-03-15 00:10:57 +0100

    common-services
    refactor code after api creation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,54 +6,12 @@ import { DataService } from './common/data.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'eurisApp';
 
   constructor(private dataService: DataService) {}
-  ngOnInit() {
-    const productExample = {
-      title :	'Beer',
-      category : 'Drinks',
-      price : 3,
-      employee :	'Mark',
-      description :	'Lager'
-    }
-    if(productExample) {
-      this.dataService.addStoreProduct(productExample).then((item: any) => {
-        console.log(item);
-      });
-    }
 
-
-
+  ngOnInit(): void {
   }
 
-  testApiCalls() {
-    this.dataService.fetchStores().then((item: any) => {
-      console.log(item);
-      this.dataService.getSingleStore().then((item: any) => {
-        console.log(item);
-        this.dataService.getStoreProducts().then((item: any) => {
-          console.log(item);
-          this.dataService.getStoreStats().then((item: any) => {
-            console.log(item);
-            const productExample = {
-              title :	'Beer',
-              category : 'Drinks',
-              price : 3,
-              employee :	'Mark',
-              description :	'Lager'
-            }
-            this.dataService.addStoreProduct(productExample).then((item: any) => {
-              console.log(item);
-              const productToDelete = '1QxR4LWyIPez1IHyV8fQ';
-              this.dataService.deleteStoreProduct(productToDelete).then((response) => {
-                console.log(response);
-              });
-            });
-          });
-        });
-      });
-    });
-  }
 }
