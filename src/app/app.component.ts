94c99451f6f6583cbcdd4093ecfe695bff4a0098
Filippo Bodini <filippo.bodini@softwave.it>
2021-03-18 22:34:55 +0100

    4-charts
    init chartjs feature with raw layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,14 @@ import {select, Store} from '@ngrx/store';
 import {deleteProduct, fetchProducts, fetchStoreInfo, saveProduct} from './store/actions';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ProductDetailInterface} from './interfaces/productDetail.interface';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
+
 export class AppComponent implements OnInit {
   mobileMenuOpened = false;
   submitNewProductHasErrors = false;
@@ -37,6 +39,7 @@ export class AppComponent implements OnInit {
     this.mobileMenuOpened = false;
     this.fetchStoreInfo();
     this.fetchProducts();
+    this.fetchProductsStats();
   }
 
   public fetchProducts(): void {
@@ -59,6 +62,30 @@ export class AppComponent implements OnInit {
       } as ProductDetailInterface;
       this.store.dispatch(saveProduct({newProduct}));
     }
+  }
+
+  public fetchProductsStats(): void {
+    this.dataService.getStoreStats().then(result => {
+      const labels = result.map(element => element.category);
+      const datasets = result.map(element => element.numberOfProducts);
+      const bgColors = result.map(() => {
+        return  'rgb(' +  Math.floor(Math.random() * 255) + ',' +
+          Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';
+      });
+      const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+      const chart = new Chart(ctx, {
+        type: 'polarArea',
+        data: {
+          labels,
+          datasets: [{
+            data: datasets,
+            backgroundColor: bgColors,
+          }],
+        }
+
+      });
+    });
+
   }
 
   public deleteProduct(id: string): void {
